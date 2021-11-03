c912916a1ae3a6f9f5a483f7401775449aecc2c8
andresiglesias1996 <92554302+andresiglesias1996@users.noreply.github.com>
2021-11-02 21:15:27 -0300

    nav
    menu queda responsive (faltan mas cambios)
    se agrega iconos

--- gestion_de_farmacias/settings.py
+++ gestion_de_farmacias/settings.py
@@ -84,15 +84,15 @@ WSGI_APPLICATION = 'gestion_de_farmacias.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-"""
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -103,7 +103,7 @@ DATABASES = {
         'PORT': '5432', #'DATABASE_PORT': '5432',
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
